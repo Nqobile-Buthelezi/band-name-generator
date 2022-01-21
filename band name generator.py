d03b77e793be762c3e51f31a4f2544e6ce94d124
Nqobile Buthelezi <nqobilethabobuthelezi@gmail.com>
2022-01-21 10:54:57 +0200

    Initial commit and complete band name generator

---
+++ band name generator.py
@@ -0,0 +1,7 @@
+print("Welcome future rockstar to the band name generator!")
+
+user_city = input("What is the name of the city you grew up in?\n")
+user_pet_name = input("What is the name of your pet?\n")
+
+band_name = user_city + " " + user_pet_name
+print("Your band name could be " + band_name + ".")
